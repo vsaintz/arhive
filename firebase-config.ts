50b3732963f400cc18ed054af8ecb91bc14f2a92
vsaintz <vsaintz.dev@gmail.com>
2025-06-04 15:48:57 +0530

    firebase-config.ts

---
+++ firebase-config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyARKfIsD2WYBW863PCIftcskdtC1vDsEDk",
+    authDomain: "arhive-966fc.firebaseapp.com",
+    projectId: "arhive-966fc",
+    storageBucket: "arhive-966fc.firebasestorage.app",
+    messagingSenderId: "127698288890",
+    appId: "1:127698288890:web:cd0919ab627a5cfcf84943",
+    measurementId: "G-P18E186XPZ"
+};
+
+const app = initializeApp(firebaseConfig);
