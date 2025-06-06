fbab337f9f89ae7ab1779ecb598368564fa8857e
vsaintz <vsaintz.dev@gmail.com>
2025-06-06 17:02:26 +0530

    camera component

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import React from 'react';
+import { Linking, Text, View } from 'react-native';
 
 export default function CameraScreen() {
+  useFocusEffect(
+    React.useCallback(() => {
+      Linking.openURL('https://prayag0895.github.io/3dq3teffe');
+      return () => { };
+    }, [])
+  );
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Camera Screen</Text>
+      <Text>Opening Camera URL...</Text>
     </View>
   );
 }
