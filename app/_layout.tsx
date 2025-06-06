6163f9cfdc7d2e2508346c13e1c71c1efd02c577
vsaintz <vsaintz.dev@gmail.com>
2025-06-06 16:53:01 +0530

    stack screen for (auth) and (tabs)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(tabs)" />
     </Stack>
+
   );
 }
