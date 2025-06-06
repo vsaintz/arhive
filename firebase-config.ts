720d7deb399603940c7a34fa4071af079d8608c8
vsaintz <vsaintz.dev@gmail.com>
2025-06-06 14:38:27 +0530

    firebase-config.ts

--- firebase-config.ts
+++ firebase-config.ts
@@ -1,13 +1,16 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARKfIsD2WYBW863PCIftcskdtC1vDsEDk",
-    authDomain: "arhive-966fc.firebaseapp.com",
-    projectId: "arhive-966fc",
-    storageBucket: "arhive-966fc.firebasestorage.app",
-    messagingSenderId: "127698288890",
-    appId: "1:127698288890:web:cd0919ab627a5cfcf84943",
-    measurementId: "G-P18E186XPZ"
+  apiKey: "AIzaSyARKfIsD2WYBW863PCIftcskdtC1vDsEDk",
+  authDomain: "arhive-966fc.firebaseapp.com",
+  projectId: "arhive-966fc",
+  storageBucket: "arhive-966fc.appspot.com",
+  messagingSenderId: "127698288890",
+  appId: "1:127698288890:web:cd0919ab627a5cfcf84943",
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
