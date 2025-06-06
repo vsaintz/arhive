1c72dc1a80f001edb0339365069da8a8054a8a9b
vsaintz <vsaintz.dev@gmail.com>
2025-06-06 16:58:01 +0530

    feat(tabs): home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,40 +1,40 @@
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
 import type { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
-import { styles } from '@/styles/TabStyles';
+import { styles } from '@/styles/CustomTabStyles';
 import CustomTabBarButton from '@/components/CustomTabBarButton';
 
 export default function TabLayout() {
-    return (
-        <Tabs
-            screenOptions={({ route }): BottomTabNavigationOptions => ({
-                headerShown: false,
-                tabBarShowLabel: false,
-                tabBarStyle: styles.tabBar,
-                tabBarActiveTintColor: '#111827',
-                tabBarInactiveTintColor: '#9CA3AF',
-                tabBarButton: (props) => <CustomTabBarButton {...props} />,
-                tabBarIcon: ({ color }) => {
-                    let iconName: keyof typeof Ionicons.glyphMap;
+  return (
+    <Tabs
+      screenOptions={({ route }): BottomTabNavigationOptions => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: styles.tabBar,
+        tabBarActiveTintColor: '#111827',
+        tabBarInactiveTintColor: '#9CA3AF',
+        tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        tabBarIcon: ({ color }) => {
+          let iconName: keyof typeof Ionicons.glyphMap;
 
-                    switch (route.name) {
-                        case 'index':
-                            iconName = 'home-outline';
-                            break;
-                        case 'camera':
-                            iconName = 'camera-outline';
-                            break;
-                        case 'profile':
-                            iconName = 'person-outline';
-                            break;
-                        default:
-                            iconName = 'ellipse-outline';
-                            break;
-                    }
+          switch (route.name) {
+            case 'index':
+              iconName = 'home-outline';
+              break;
+            case 'camera':
+              iconName = 'camera-outline';
+              break;
+            case 'profile':
+              iconName = 'person-outline';
+              break;
+            default:
+              iconName = 'ellipse-outline';
+              break;
+          }
 
-                    return <Ionicons name={iconName} size={24} color={color} />;
-                },
-            })}
-        />
-    );
+          return <Ionicons name={iconName} size={24} color={color} />;
+        },
+      })}
+    />
+  );
 }
