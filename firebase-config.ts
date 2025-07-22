b9e573b63506519b633b772ef1b2a93c63684bba
vsaintz <vsaintz.dev@gmail.com>
2025-07-22 21:24:35 +0530

    refactor and enhance styles/ for improved UI consistency

--- firebase-config.ts
+++ firebase-config.ts
@@ -6,12 +6,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getReactNativePersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARKfIsD2WYBW863PCIftcskdtC1vDsEDk",
-  authDomain: "arhive-966fc.firebaseapp.com",
-  projectId: "arhive-966fc",
-  storageBucket: "arhive-966fc.appspot.com",
-  messagingSenderId: "127698288890",
-  appId: "1:127698288890:web:cd0919ab627a5cfcf84943",
+  // Firebase API Key
+ 
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
